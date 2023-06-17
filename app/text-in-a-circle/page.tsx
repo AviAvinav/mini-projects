bfff6351a9217d55ca681201acd0d15582817c14
Avi Avinav <aviavinav2004@gmail.com>
2023-06-17 19:37:58 +0530

    still figuring out this one

---
+++ app/text-in-a-circle/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p className="font-bold text-xl">
+        This is the text that I want to circlize(?) | This is the text that I
+        want to circlize(?)
+      </p>
+    </div>
+  );
+}
