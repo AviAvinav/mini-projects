2e24b1c1472c2c823d0119a97a749c0b26b2aefc
Avi Avinav <aviavinav2004@gmail.com>
2023-12-30 11:08:02 +0530

    fixed typescript bug

--- app/skewed-scrolling/page.tsx
+++ app/skewed-scrolling/page.tsx
@@ -3,37 +3,38 @@
 import { useEffect, useRef } from "react";
 
 export default function Page() {
-	const text1 = useRef();
-	const text2 = useRef();
-	const text3 = useRef();
-	useEffect(() => {
-		window.onscroll = function() {
-			text1.current.style.transform = `skewX(${Math.cos(window.scrollY / 50) * 30
-				}deg)`;
-		};
-	}, []);
-	return (
-		<>
-			<div className="flex items-center justify-center h-screen">
-				<h1 ref={text1} className="text-2xl font-semibold">
-					I will italicize on scrolling
-				</h1>
-			</div>
-			<div className="flex items-center justify-center h-screen">
-				<h1 ref={text2} className="text-2xl font-semibold">
-					I will italicize on scrolling
-				</h1>
-			</div>
-			<div className="flex items-center justify-center h-screen">
-				<h1 ref={text3} className="text-2xl font-semibold">
-					I will italicize on scrolling
-				</h1>
-			</div>
-			<div className="flex items-center justify-center h-screen">
-				<h1 className="text-2xl font-semibold">
-					I will italicize on scrolling
-				</h1>
-			</div>
-		</>
-	);
+  const text1 = useRef<HTMLHeadingElement>(null);
+  const text2 = useRef<HTMLHeadingElement>(null);
+  const text3 = useRef<HTMLHeadingElement>(null);
+  useEffect(() => {
+    window.onscroll = function () {
+      text1.current!.style.transform = `skewX(${-Math.abs(
+        Math.sin(window.scrollY / 50) * 30
+      )}deg)`;
+    };
+  }, []);
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <h1 ref={text1} className="text-2xl font-semibold">
+          I will italicize on scrolling
+        </h1>
+      </div>
+      <div className="flex items-center justify-center h-screen">
+        <h1 ref={text2} className="text-2xl font-semibold">
+          I will italicize on scrolling
+        </h1>
+      </div>
+      <div className="flex items-center justify-center h-screen">
+        <h1 ref={text3} className="text-2xl font-semibold">
+          I will italicize on scrolling
+        </h1>
+      </div>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-2xl font-semibold">
+          I will italicize on scrolling
+        </h1>
+      </div>
+    </>
+  );
 }
